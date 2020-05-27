46f5ea8874cb5e5f1a82b01dbf9c332809fae31f
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2020-05-27 15:46:56 -0500

    set mobile styles for about page hero and sign up section

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Button } from "carbon-components-react";
+import small from "../../assets/images/image-background-hero-w_300.jpeg";
+import medium from "../../assets/images/image-background-hero-w_400.jpeg";
+import large from "../../assets/images/image-background-hero-w_600.jpeg";
+import medlarge from "../../assets/images/image-background-hero-w_800.jpeg";
+import xlarge from "../../assets/images/image-background-hero-w_1200.jpeg";
+import max from "../../assets/images/image-background-hero-w_1500.jpeg";
+
+const Hero = () => {
+  return (
+    <section className="bx--row hero">
+      <div className="bx--col-lg-16 hero__image-container">
+        <picture>
+          <source srcSet={max} media="(min-width: 1500px)" />
+          <source srcSet={xlarge} media="(min-width: 1200px)" />
+          <source srcSet={medlarge} media="(min-width: 800px)" />
+          <source srcSet={large} media="(min-width: 600px)" />
+          <source srcSet={medium} media="(min-width: 400px)" />
+          <img
+            src={small}
+            alt="A man conducting research"
+            className="hero__image"
+          />
+        </picture>
+      </div>
+
+      <div className="bx--col-lg-16 hero__text-container">
+        <h4 className="hero__title">
+          Let's shape the future of IBM products, together
+        </h4>
+        <p className="hero__subtitle">
+          We want to hear about your user experiences - what's working and
+          what's not working. Join our user research studies and tell us what
+          you think.
+        </p>
+        <Button className="button--hero">Sign up</Button>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,67 @@
 import React from "react";
+import Hero from "../../components/Hero";
+import impact from "../../assets/svg/impact.svg";
+import coCreation from "../../assets/svg/co-creation.svg";
+import exclusiveContent from "../../assets/svg/exclusive-content.svg";
+import compensation from "../../assets/svg/compensation.svg";
+
+const signupSectionData = [
+  {
+    title: "Exclusive access",
+    body:
+      "Get a sneak peek at what we're working on in global enterprise software solutions.",
+    note: "",
+    image: exclusiveContent,
+    alt: "animated web pages"
+  },
+  {
+    title: "Co-creation",
+    body:
+      "Connect with our friendly researchers who want to hear about your experience.",
+    note: "",
+    image: coCreation,
+    alt: "animated text bubble"
+  },
+  {
+    title: "Impact",
+    body: "Directly impact the future of the product.",
+    note: "",
+    image: impact,
+    alt: "animated light bulb"
+  },
+  {
+    title: "Compensation",
+    body: "Receive compensation for your time in user research studies.*",
+    note: "* We are prohibited from compensating government owned entities.",
+    image: compensation,
+    alt: "animated star"
+  }
+];
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="bx--grid bx--grid--full-width about-page">
+      <Hero />
+      <section className="bx--row sign-up">
+        <h4 className="bx--col-lg-4 sign-up__section-title">Why sign up?</h4>
+        <div className="bx--row sign-up__cards-container">
+          {signupSectionData.map(d => (
+            <div
+              className="sign-up__card bx--col-lg-3 bx--col-md-4 bx--col-sm-8"
+              key={d.title}
+            >
+              <img className="sign-up__card-image" src={d.image} alt={d.alt} />
+              <h4 className="sign-up__card-title">{d.title}</h4>
+              <p className="sign-up__card-body">{d.body}</p>
+              {d.note && (
+                <span className="sign-up__card-note">
+                  <em>{d.note}</em>
+                </span>
+              )}
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
