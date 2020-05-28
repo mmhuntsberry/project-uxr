11c85785e5bde63c57acfa0a767228ebe28ff5e7
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2020-05-28 13:51:10 -0500

    set up banner for faq page

--- src/pages/FAQ/FAQ.js
+++ src/pages/FAQ/FAQ.js
@@ -2,9 +2,19 @@ import React from "react";
 
 const FAQ = () => {
   return (
-    <div>
-      <h1>FAQ</h1>
-    </div>
+    <>
+      <section className="bx--row section section--gradient hero--faq">
+        <div className="hero__text-container hero__text-container--faq">
+          <h4 className="hero__title hero__title--faq">
+            Frequently Asked Questions
+          </h4>
+          <p className="hero__subtitle hero__subtitle--faq">
+            Here are some common questions and answers about our exclusive
+            research programs.
+          </p>
+        </div>
+      </section>
+    </>
   );
 };
 
