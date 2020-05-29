f18fe563fc79b0413b69d40f8223c397b9f5a265
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2020-05-29 17:27:39 -0500

    removed bloat from template starter create react app files

--- src/index.js
+++ src/index.js
@@ -5,13 +5,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./index.scss";
 
 import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
-
-// import "core-js/modules/es7.array.includes";
-// import "core-js/modules/es6.array.fill";
-// import "core-js/modules/es6.string.includes";
-// import "core-js/modules/es6.string.trim";
-// import "core-js/modules/es7.object.values";
 
 ReactDOM.render(
   <Router>
@@ -19,8 +12,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
