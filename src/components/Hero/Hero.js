d59637bfa59357dce627a8f8c8fe906187806e58
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2020-05-29 10:24:16 -0500

    refactor hero component after save :(

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -34,7 +34,7 @@ const Hero = () => {
           what's not working. Join our user research studies and tell us what
           you think.
         </p>
-        <Button className="button--hero">Sign up</Button>
+        <Button className="button button--hero">Sign up</Button>
       </div>
     </section>
   );
