09151129ea45c432d463748030d918f0d7ae9589
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2020-06-01 12:26:16 -0500

    remove react snap

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import "./index.scss";
 
 import App from "./components/App";
 
-const rootElement = document.getElementById("root");
-
-if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrate(
-    <Router>
-      <App />
-    </Router>
-  ),
-    rootElement;
-} else {
-  ReactDOM.render(
-    <Router>
-      <App />
-    </Router>,
-    rootElement
-  );
-}
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
