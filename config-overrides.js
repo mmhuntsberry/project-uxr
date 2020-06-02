de347fe007a1febe03b0941653e71c2e9aebe1ab
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2020-06-02 09:59:15 -0500

    add changes to config override

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,18 @@
 const PrerenderSPAPlugin = require("prerender-spa-plugin");
 const path = require("path");
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 
 module.exports = (config, env) => {
   if (env === "production") {
     config.plugins = config.plugins.concat([
       new PrerenderSPAPlugin({
-        routes: ["/", "/faq"],
-        staticDir: path.join(__dirname, "build")
+        routes: ["/index.html", "/faq/index.html"],
+        staticDir: path.join(__dirname, "build"),
+        // Workaround for async
+        renderer: new Renderer({
+          renderAfterDocumentEvent: "render-event"
+        }),
+        headless: true
       })
     ]);
   }
